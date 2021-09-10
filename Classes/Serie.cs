07cbf69c2557bbc27f73999dc026581bc67c99e1
GabrielCarfe <gabrielcarfepro@gmail.com>
2021-09-10 13:56:20 -0300

    Arquivos base movidos para a pasta principal do projeto

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -14,6 +14,8 @@ namespace DIO.Series
 
         private int Ano { get; set; }
 
+        private bool Excluido { get; set; }
+
         public Serie(int id, Genero genero, string titulo, string descricao, int ano)
         {
             this.Id = id;
@@ -21,6 +23,7 @@ namespace DIO.Series
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
@@ -30,6 +33,7 @@ namespace DIO.Series
             retorno += "Título: " + this.Titulo + Environment.NewLine;
             retorno += "Descrição: " + this.Descricao + Environment.NewLine;
             retorno += "Ano: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido;
             return retorno;
         }
 
@@ -41,6 +45,16 @@ namespace DIO.Series
         public int retornaId()
         {
             return this.Id;
+        }
+
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
         }
 
     }
