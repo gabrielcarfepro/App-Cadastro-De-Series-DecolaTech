30ad2a8101d2108767cd787980e0933bb7ad36a2
GabrielCarfe <gabrielcarfepro@gmail.com>
2021-09-10 01:53:52 -0300

    Adição das seguintes classes: EntidadeBase.cs, Serie.cs, SerieRepositorio.cs. Adição do Enum Genero.cs, e da interface IRepositorio.cs - Criação dos métodos e passando implementação nas classes 'filhas' da classe abstrata

--- DIO.Series/Program.cs
+++ DIO.Series/Program.cs
@@ -4,8 +4,10 @@ namespace DIO.Series
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            //Serie meuObejto = new Serie();
             Console.WriteLine("Hello World!");
         }
     }
